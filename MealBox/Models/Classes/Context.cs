b976dee3ccc47ad7ecb1070fa3aa8f9cd1195e57
Burak Güllüler <85554705+buraxta@users.noreply.github.com>
2024-12-26 20:49:46 +0300

    Update Context.cs

--- MealBox/Models/Classes/Context.cs
+++ MealBox/Models/Classes/Context.cs
@@ -1,4 +1,4 @@
-﻿
+
 
 using Microsoft.EntityFrameworkCore;
 
@@ -8,12 +8,13 @@ namespace MealBox.Models.Classes
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Category> Categorys { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
 
-  
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server= DESKTOP-520AD7C\\SQLEXPRESS; database = MealBoxDB;integrated security =true; TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer("Server=BURAXTA\\SQLEXPRESS;Database=Yemek;Trusted_Connection=True;TrustServerCertificate=True");
         }
     }
 
